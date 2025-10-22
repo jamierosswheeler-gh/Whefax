765ff554ce48cd9d6de68cb86de0c939c532c3ff
jamierosswheeler-gh <jamierosswheeler@gmail.com>
2025-10-22 04:19:20 +0100

    last update 10.1

--- sw.js
+++ sw.js
@@ -1,24 +1,31 @@
-const CACHE_NAME="whefax-v10";
-const FILES=[
- "index.html","styles.css","user.js","backend.js",
- "manifest.json","assets/header.svg","data/deals.json"
+const CACHE_NAME = "whefax-v10.1";
+const FILES_TO_CACHE = [
+  "/",
+  "index.html",
+  "styles.css",
+  "user.js",
+  "backend.js",
+  "manifest.json",
+  "assets/header.svg",
+  "assets/favicon.png"
 ];
-self.addEventListener("install",e=>{
- e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES)));
- self.skipWaiting();
+
+self.addEventListener("install", e=>{
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(c=>c.addAll(FILES_TO_CACHE))
+  );
 });
-self.addEventListener("activate",e=>{
- e.waitUntil(caches.keys().then(k=>Promise.all(k.map(x=>x!==CACHE_NAME&&caches.delete(x)))));
- self.clients.claim();
+
+self.addEventListener("fetch", e=>{
+  e.respondWith(
+    caches.match(e.request).then(r=>r || fetch(e.request))
+  );
 });
-self.addEventListener("fetch",e=>{
- e.respondWith(
-  caches.match(e.request).then(r=>{
-   const f=fetch(e.request).then(res=>{
-    if(res&&res.ok)caches.open(CACHE_NAME).then(c=>c.put(e.request,res.clone()));
-    return res;
-   }).catch(()=>r);
-   return r||f;
-  })
- );
+
+self.addEventListener("activate", e=>{
+  e.waitUntil(
+    caches.keys().then(keys=>Promise.all(
+      keys.map(k=>k!==CACHE_NAME && caches.delete(k))
+    ))
+  );
 });
